afba331b5483cb486d3c26eda1f1792245ae001d
Jonas Kjeldsen <jonas.lykke.kjeldsen@gmail.com>
2026-05-27 17:04:48 +0200

    fixed FrameGenerator, transmitted NoneType, now transmit raw frame

--- FrameGenerator/SDRtransmitTCP.py
+++ FrameGenerator/SDRtransmitTCP.py
@@ -5,7 +5,7 @@ def transmitData(packet,host="127.0.0.1",port=52001):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((host, port))
         s.send(packet)
-        print(f"Sent {len(packet)} byte PV1 Management Action frame to GNU Radio")
+        print(f"Sent {len(packet)} byte PV0 Management Action frame to GNU Radio")
     except Exception as e:
         print(f"Socket error: {e}")
     finally:
--- FrameGenerator/main.py
+++ FrameGenerator/main.py
@@ -5,10 +5,10 @@ from scapy.all import wrpcap
 if __name__ == "__main__":
     print("Frame Generator")
     print("=========================================================================")
-    print("This script was created as a part of a master thesis project to generate frames for testing and evaluation of WiFi HaLow networks.")
-    print("The script is designed to generate frames and allowing for saving them to a pcap file or transmitting them directly to GNU Radio for over-the-air transmission using a Software Defined Radio (SDR).")
-    print("For over the air transmission using a SDR, please ensure that GNU Radio is running and ready to receive frames on the specified port.")
-    print("by default, the script will attempt to connect to GNU Radio on localhost (127.0.0.1:52001), but you can modify the host and port in the transmitData function if needed.")
+    print("This script was created as a part of a master thesis project to generate frames for testing and evaluation of WiFi HaLow networks.\n")
+    print("The script is designed to generate frames and allowing for saving them to a pcap file or transmitting them directly to GNU Radio for over-the-air transmission using a Software Defined Radio (SDR).\n")
+    print("For over the air transmission using a SDR, please ensure that GNU Radio is running and ready to receive frames on the specified port.\n")
+    print("by default, the script will attempt to connect to GNU Radio on localhost (127.0.0.1:52001), but you can modify the host and port in the transmitData function if needed.\n")
     print("=========================================================================")
     raw_frame = None
     continue_choice = None
@@ -23,8 +23,7 @@ if __name__ == "__main__":
             match (frame_choice):
                 case "1":
                     invalid_frame_choice = False
-                    frame = frames.start_deauthentication_frame_generator()
-                    continue
+                    raw_frame = frames.start_deauthentication_frame_generator()
                 case _:
                     print("Invalid choice. Please enter a valid number.")
                     continue
@@ -36,11 +35,13 @@ if __name__ == "__main__":
                 pcap_name = input("Pcap name:")
                 path = input("Enter the path to save the pcap file (e.g., 'C:/Users/username/Desktop/'): ")
                 full_path = f"{path}{pcap_name}.pcap"
-
                 wrpcap(full_path, raw_frame)
+
+
             elif mode_choice == "2" and not invalid_frame_choice:
                 print("transmit raw frames")
                 transmitData(raw_frame)
+
 
             print("\nDo you want to select another frame type? (y/n)")
             continue_choice = input("Enter 'y' for yes or 'n' for no: ")
