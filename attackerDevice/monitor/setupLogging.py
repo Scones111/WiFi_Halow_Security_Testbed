12cc067c867ee0c91cd0f09912dfb868efa5ca8c
Jonas Kjeldsen <jonas.lykke.kjeldsen@gmail.com>
2026-06-01 15:31:10 +0200

    fixed filter variable

--- attackerDevice/monitor/setupLogging.py
+++ attackerDevice/monitor/setupLogging.py
@@ -18,7 +18,7 @@ DEFAULT_PASSWORD = data["Monitor"][0]["password"]
 
 DEFAULT_INTERFACE = "morse0"
 
-WIRESHARK_FILTER = "'(wlan || tcp) && !arp && !stp && !rldp && !mdns && !udp && !icmpv6 && !igmp && !ipv6'"
+WIRESHARK_FILTER = "(wlan || tcp) && !arp && !stp && !rldp && !mdns && !udp && !icmpv6 && !igmp && !ipv6"
 
 client = None
 wireshark = None
