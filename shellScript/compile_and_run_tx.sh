b9e012e707f8ba0d2d4d741dbd91e307d47d39ea
Scones111 <jonas.lykke.kjeldsen@gmail.com>
2026-06-10 16:32:10 +0200

    fixed compile_and_run_tx.sh, would work using normal python3 has to use /usr/bin/python3

--- shellScript/compile_and_run_tx.sh
+++ shellScript/compile_and_run_tx.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
-exec_folder="$(cd "$(dirname "$0")" && pwd)"
+exec_folder="$(cd .. && pwd)"
 grc_folder="$exec_folder/gr-ieee802-11ah/examples"
+
+
 
 PHY="halow_phy_hier.grc"
 TX="halow_tx.grc"
@@ -14,4 +16,5 @@ grcc -o "$HOME/.local/state/gnuradio" "$PHY"
 grcc -o "$grc_folder" "$TX"
 
 echo "[INFO] Running TX..."
-python3 "$grc_folder/halow_tx.py"
+/usr/bin/python3 "$grc_folder/halow_tx.py"
+
