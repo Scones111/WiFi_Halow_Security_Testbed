f716fbbb4064e4989a92f3a7aea1d61a1686a900
simonhermansen <simonhermansen5dk@gmail.com>
2026-06-22 17:30:24 +0200

    correct import path

--- attackerDevice/monitor/setupMetricClient.py
+++ attackerDevice/monitor/setupMetricClient.py
@@ -6,7 +6,7 @@ import utils
 import time
 import sys
 import os
-import centralized_metrics_logger
+from attackerDevice.monitor import centralized_metrics_logger
 
 def check_con_devices():
     con_devices = {device["name"]:False for device in utils.load_json()["STA"]}
